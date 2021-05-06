65022c8662028f0bc1127b0dac9972cdc503baed
Ryan Cross <rcross@kyruus.com>
2021-05-06 15:00:27 -0400

    updating work so far

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask
+from flask import Flask, jsonify, request, render_template
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
+
+items = [
+    {
+        'name': 'Test Item',
+        'price': .99
+    }
+]
 
 
-@app.route('/') # 'https://www.google.com'
-def home():
-    return "Hello, World"
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if name == item['name']:
+                return item
+        return {'Item': None}, 404
 
+    def post(self, name):
+        request_data = request.get_json()
+        item = {'name': name, 'price': request_data['price']}
+        items.append(item)
+        return item, 201
 
-app.run(port=5000)
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
